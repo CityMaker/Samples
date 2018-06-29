617cdae3d641f2642b95873c62a47aae63903baa
kateyuanying <kateyuanying@163.com>
2018-06-29 16:53:35 +0800

    add Connect addin example

---
+++ CLI/beginner/CityMakerConnect.AddIn.Example/CityMakerConnect.AddIn.Example/Command/LoginCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CityMakerExplorer.AddIn.Core;
+using CityMakerExplorer.WorkSpace;
+using System.Windows.Forms;
+
+namespace CityMakerExplorer.AddIn.Example
+{
+    class LoginCommand : AbstractCommand
+    {
+        public override void RestoreEnv()
+        {
+                
+        }
+        public override void Run(object sender, EventArgs e)
+        {
+            CommandManager.Push(this);
+            LoginForm form = new LoginForm();
+            if (form.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                MessageBox.Show("");
+            }
+        }
+    }
+}
